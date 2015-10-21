77a90def598dedab5476acb7f9629468ecd351cc
lee627 <lee627@mail.chapman.edu>
2015-10-21 15:55:42 -0700

    Added scrolling background

--- FlappyShibe/FlappyShibe/GameScene.swift
+++ FlappyShibe/FlappyShibe/GameScene.swift
@@ -9,9 +9,30 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var background = SKSpriteNode();
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
+        // Created texture
+        let backgroundTexture = SKTexture(imageNamed: "Background.png");
+
+        let moveBackground = SKAction.moveByX(-backgroundTexture.size().width, y: 0, duration: 9);
+        let replaceBackground = SKAction.moveByX(backgroundTexture.size().width, y: 0, duration: 0);
+        let moveBackgroundForever = SKAction.repeatActionForever(SKAction.sequence([moveBackground, replaceBackground]));
+        
+        for var i: CGFloat = 0; i < 3; i++ {
+            
+            // Applied Texture to that particular sprite node
+            background = SKSpriteNode(texture: backgroundTexture);
+            background.position = CGPoint(x: backgroundTexture.size().width / 2 + backgroundTexture.size().width * i, y: CGRectGetMidY(self.frame));
+            // Set height equal to the height of the screen
+            background.size.height = self.frame.height;
+            background.runAction(moveBackgroundForever);
+            self.addChild(background);
+            
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
