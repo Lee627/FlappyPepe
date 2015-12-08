543e4e0cd6d3ad3e502d90d67794d7b365d5b5ad
samy achour <sachour96@gmail.com>
2015-12-08 01:41:48 -0800

    Added pause button programmatically

--- FlappyShibe/FlappyShibe/GameViewController.swift
+++ FlappyShibe/FlappyShibe/GameViewController.swift
@@ -11,15 +11,22 @@ import SpriteKit
 
 class GameViewController: UIViewController {
     
-    //Variable to store if the user touched the screen yet to start the game
-    var userStarted = false
-
+    //Variable to store if the game has been paused or not
+    var gamePaused = true
+    
+    //Has the user started their first game yet
+    var start = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let skView = self.view as! SKView
 
         if let scene = GameScene(fileNamed:"GameScene") {
+            
+            if !start{
+                
             // Configure the view.
-            let skView = self.view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
             
@@ -28,28 +35,61 @@ class GameViewController: UIViewController {
             
             /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
-            
+                
             skView.presentScene(scene)
+                
+            //Pause initially
             skView.paused = true
-            
-            //if the user touched the screen to start the game
-            if userStarted {
-                skView.paused = false
+                
+            //The user started their first game
+            start = true
+                
+            let button   = UIButton(type: UIButtonType.System) as UIButton
+            button.frame = CGRectMake(30, 30, 30, 30)
+            button.backgroundColor = UIColor.blackColor()
+            button.setTitle("||", forState: UIControlState.Normal)
+            button.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+                
+            self.view.addSubview(button)
+                
             }
+            
+        }
+        
+        //If game is paused, pause the view
+        if gamePaused {
+            
+            skView.paused = true
+        }
+        
+        //If game is not paused, unpause the view
+        if !gamePaused {
+            
+            skView.paused = false
             
         }
     }
     
-    //if the user touches the screen anywhere
+    //If the user presses the pause button
+    @IBAction func buttonAction(sender: AnyObject) {
+        
+        gamePaused = true
+        viewDidLoad()
+        
+    }
+    
+    //If the user touches the screen anywhere
     override func touchesBegan(touches: Set<UITouch>?, withEvent event: UIEvent?) {
         
-        if userStarted == false {
-            userStarted = true
+        if gamePaused == true {
+            
+            gamePaused = false
             viewDidLoad()
+            
         }
         
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
